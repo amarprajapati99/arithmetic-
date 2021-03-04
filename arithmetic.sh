0e092994946643903f5a4958045e8f3885b9fb73
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-04 12:22:34 +0530

    show dic result

--- arithmetic.sh
+++ arithmetic.sh
@@ -11,6 +11,16 @@ echo $cal
 compute=$(( a*b+c ))
 echo $compute
 compute1=$(( c + a / b ))
-echo $compute
+echo $compute1
 res=$(( a + b % c ))
 echo $res
+declare -A  dic
+dic[1]="$cal"
+dic[2]="$compute"
+dic[3]="$compute1"
+dic[4 ]="$res"
+echo "First calclution result is " ${dic[1]}
+echo "Second calclution result is " ${dic[2]}
+echo "Third calclution result is " ${dic[3]}
+
+
